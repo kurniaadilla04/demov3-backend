28eb6eca2776be3d1a156bac9a418be1e3a13d42
kurniaadilla04 <kurniaadll.adill@gmail.com>
2025-08-01 16:19:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,9 @@ def predict():
             "kematangan": result['class'].lower()
         }), 200
 
+#if __name__ == '__main__':
+#    app.run(debug=True)
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # gunakan PORT dari Railway
+    app.run(host='0.0.0.0', port=port)
+
